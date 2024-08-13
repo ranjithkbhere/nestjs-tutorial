2d799c145d7ed4ab1fd4fcf5b66e8265acf0ef1c
Ranjith Kumar <ranjithkb.here@gmail.com>
2024-08-13 21:45:35 +0530

    Changing message 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!, Added Config files to Source';
+    return 'Hello World!, SSH host with public IP is working';
   }
 }
