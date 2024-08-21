477145ee58beba0922a0ff609501c941bde30d75
Ranjith Kumar <ranjithkb.here@gmail.com>
2024-08-21 12:41:21 +0530

    Github Webhook

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!Webhook testing';
+    return 'Hello World!Webhook Demo';
   }
 }
